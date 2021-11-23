464b7d9586610cad785dd7bf3df2496451f092b2
MuhammadDinoRahmanHakim <92322621+MuhammadDinoRahmanHakim@users.noreply.github.com>
2021-11-23 11:46:59 +0700

    Add files via upload

---
+++ ProjekCRUD/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+// untuk menyimpan konstans database
+define('BASEURL', 'http://localhost/kuliah/ProjekCRUD/public/');
+// memindah aplikasi sehingga cukup merubah fike tersebyt
+
+// db host untuj menyimpan nama host nya
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'projectPbo');
---
+++ ProjekCRUD/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'dinorh_hakim';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
